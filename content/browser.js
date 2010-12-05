ae4fdcd5b68aa18197718a811a729ac4e55ef21c
Nathan R. Yergler <nathan@yergler.net>
2010-12-05 15:31:32 -0800

    Fixes for event listening and UI updates.
    * Listen for state/location events across all tabs
    * Only manipulate the UI state if the document is visible

--- content/browser.js
+++ content/browser.js
@@ -127,68 +127,73 @@ var gCcHandler = {
     
     showIcon : function(document) {
 	const objects = ccffext.objects.extract(document);
+
 	this._icon.hidden = false;
 	gCcHandler._icon.setAttribute("tooltiptext",
 				  ccffext.l10n.get("icon.title.label",
 						   objects.length));
     },
 
-    showIconIfLicensed : function(document) {
-	if (document instanceof HTMLDocument &&
-	    ccffext.objects.licenseCached(document))
-	{
-	    // Show the icon back if the document is cached and contains licensed objects
-	    this.showIcon(document);
-	} else {
-	    this.hideIcon();
+    showIconIfLicenseInfo : function(document) {
+
+	// if no document is provided, default to the active document
+	if ("undefined" == typeof document) {
+	    document = gBrowser.contentDocument;
+	}
+
+	// if this is the active document, hide the icon
+	if (gBrowser.contentDocument == document)
+	    gCcHandler.hideIcon();
+
+	if (document instanceof HTMLDocument) {
+	    ccffext.objects.callbackify(
+		document, 
+		function(document,objects) {
+		    if (gBrowser.contentDocument == document) 
+			gCcHandler.showIcon(document);
+		},
+		function(document) {
+		    // license not cached
+		    ccffext.objects.parse(document.location.href, document,
+					  RDFA, XH);
+		});
 	}
     }
-
 };
 
 /**
-* Register a page load listener that would look for licensing
-* information on pages properly responding for it. Gathered
-* information is cached for performance reasons		 
-**/
+ *  Register window load listener which adds event listeners for tab,
+ *  location, and state changes. 
+ **/
 window.addEventListener("load",function() {
-    gBrowser.addProgressListener({
-	onLocationChange : function(progress,request,uri) {
-	    // A tab is opened, closed, or switched to
 
+    gBrowser.addEventListener(
+	"TabSelect", 
+	function(e) { gCcHandler.showIconIfLicenseInfo();}, false);
+    gBrowser.tabContainer.addEventListener(
+	"TabSelect", 
+	function(e) { gCcHandler.showIconIfLicenseInfo();}, false);
+
+    gBrowser.addTabsProgressListener({
+	onLocationChange : function(browser, progress,request,uri) {
+
+	    // A tab is opened, closed, or switched to
 	    // Show the location bar icon if license information is present
-	    gCcHandler.showIconIfLicensed(progress.DOMWindow.document);
+	    gCcHandler.showIconIfLicenseInfo(progress.DOMWindow.document);
+
 	},
 	
-	onStateChange : function(progress,request,flag,status) {
+	onStateChange : function(browser, progress,request,flag,status) {
 	    
 	    // A document in an existing tab stopped loading
 	    if (flag & Components.interfaces.nsIWebProgressListener.STATE_STOP)
 	    {
 		const doc = progress.DOMWindow.document;
 		
-		if (doc instanceof HTMLDocument) 
-		{
-		    // Parse the information and put it to cache.
-		    // Show the icon back if the document contains licensed objects
-		    ccffext.objects.callbackify(
-			doc,
-			function(doc) {
-			    // license cached
-			    gCcHandler.showIcon(doc);
-			},
-			function(doc) {
-			    // license not cached
-			    ccffext.objects.parse(doc.location.href, doc,
-						  RDFA, XH);
-			});
-
-		}
+		gCcHandler.showIconIfLicenseInfo(progress.DOMWindow.document);
 	    }
 	},
-	onProgressChange: function() {},
-	onStatusChange: function() {},
-	onSecurityChange: function() {}
 	
-    }, Components.interfaces.nsIWebProgress.NOTIFY_LOCATION);
+    });
+
 },false);
