9f5719c8b5b8b676388041a4847fc16eb1ff1a40
Nathan R. Yergler <nathan@yergler.net>
2011-01-08 14:51:43 -0800

    Refactored licenseloader into a singleton object, licenses.
    This allows us to store state in the object, as well as provides
    better protection for private members.

--- module/license.js
+++ module/license.js
@@ -1,48 +1,50 @@
-var EXPORTED_SYMBOLS = ["licenseloader"];
+var EXPORTED_SYMBOLS = ["licenses"];
 
 Components.utils.import("resource://ccffext/ccffext.js");
 
-var licenseloader = {
-    timer : Components.classes["@mozilla.org/timer;1"]
-        .createInstance(Components.interfaces.nsITimer),
-    license_frame : null,
-    queue : new Array(),
-    working : false,
-    _current_callback : null,
+var licenses = new function Licenses() {
 
-    init : function (license_frame) {
+    var timer = Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer);
+    var license_frame = null, queue = new Array(), working = false, current_callback = null;
+    that = this;
 
-	if (this.license_frame == null) {
-	    // if (!license_frame.hasAttribute("ccffext_configured")) {
-	    license_frame.webNavigation.allowAuth = true;
-	    license_frame.webNavigation.allowImages = false;
-	    license_frame.webNavigation.allowJavascript = false;
-	    license_frame.webNavigation.allowMetaRedirects = true;
-	    license_frame.webNavigation.allowPlugins = false;
-	    license_frame.webNavigation.allowSubframes = false;
-	    
-	    license_frame.addEventListener(
-		"DOMContentLoaded", this._onDomLoaded, true);
-	    licenseloader.license_frame = license_frame;
-	    // license_frame.setAttribute("ccffext_configured", "true");
-	}
+    this.init = function (browser) {
 
-	licenseloader.working = false;
+	browser.webNavigation.allowAuth = true;
+	browser.webNavigation.allowImages = false;
+	browser.webNavigation.allowJavascript = false;
+	browser.webNavigation.allowMetaRedirects = true;
+	browser.webNavigation.allowPlugins = false;
+	browser.webNavigation.allowSubframes = false;
+	
+	// attach the event listener which will handle parsing licenses
+	browser.addEventListener(
+	    "DOMContentLoaded", _onDomLoaded, true);
+	
+	// store a reference to the browser
+	license_frame = browser;
+	
+	// reset the internal state flag
+	working = false;
 				
-    }, // init
+    } // init
 
-    load_license : function (license_uri, callback) {
+    this.load = function (license_uri, callback) {
 	// refuse to queue something we won't be able to handle
 	if ("string" == typeof license_uri) {
-	    licenseloader.queue.push([license_uri, callback]);
+	    queue.push([license_uri, callback]);
 	}
 
 	// and check the queue...
-	licenseloader._check_queue();
+	check_queue();
 
-    }, // load_license
+    } // load
 
-    _onDomLoaded : function (e) {
+    this.notify = function (timer) {
+	check_queue();
+    }
+
+    function _onDomLoaded (e) {
 	
 	var doc = e.originalTarget;
 	var url = doc.location.href;
@@ -51,49 +53,43 @@ var licenseloader = {
 	ccffext.objects.parse(url, doc);
 			    
 	// reset flags
-	licenseloader.working = false;
+	working = false;
 	
 	// see if there's anything else to process once we're done
-	licenseloader.timer.initWithCallback(
-	    licenseloader,
+	timer.initWithCallback(
+	    that,
 	    100, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
 
 	// call the callback when done
-	if ("function" == typeof licenseloader._current_callback)
-	    licenseloader._current_callback (url);
+	if ("function" == typeof _current_callback)
+	    _current_callback (url);
 
-    }, // _onDomLoaded
+    } // _onDomLoaded
 
-    notify : function (timer) {
-	licenseloader._check_queue();
-    },
+    var check_queue = function () {
 
-    _check_queue : function () {
-
-	if (licenseloader.queue.length > 0) {
+	if (queue.length > 0) {
 	    // see if we're currently working
-	    if (licenseloader.working == true) {
-		licenseloader.timer.initWithCallback(
-		    licenseloader,
+	    if (working == true) {
+		timer.initWithCallback(
+		    that,
 		    100, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
 		return;
 	    }
 
-	    // assert licenseloader.working == false;
-
-	    licenseloader.working = true;
+	    working = true;
 	    var license_uri;
 
-	    [license_uri, licenseloader._current_callback] = licenseloader.queue.pop();
-	    licenseloader.license_frame.webNavigation.loadURI(
+	    [license_uri, _current_callback] = queue.pop();
+	    license_frame.webNavigation.loadURI(
 		license_uri,
 		Components.interfaces.nsIWebNavigation, null, null, null);
 
 	}	   
 
-    }, // _check_queue
+    } // check_queue
 
-  log : function(message)
+    this.log = function(message)
     {
 	Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService)
 	    .logStringMessage(message);
