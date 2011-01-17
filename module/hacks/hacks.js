ceb2f31c5affeedc4e962bb589eea531d0519a88
Nathan R. Yergler <nathan@yergler.net>
2011-01-17 13:03:22 -0800

    Flickr site hack: extract attribution name and URL from the page.

--- module/hacks/hacks.js
+++ module/hacks/hacks.js
@@ -22,5 +22,31 @@ var ccffext_site_hacks = new function Hacks() {
 ccffext_site_hacks.register(
     "flickr", /^https?\:\/\/(www\.)?flickr\.com\/.*/,
     function(triples, location, document) {
-	
+
+	Components.utils.import("resource://ccffext/rdfa.js");
+
+	var author = document.evaluate("//span[@class='realname']/a",
+				       document,
+				       null,
+				       Components.interfaces.nsIDOMXPathResult.STRING_TYPE,
+				       null).stringValue;
+
+	if ("undefined" != typeof author) {
+	    triples.add(new RDFSymbol(location),
+			new RDFSymbol("http://creativecommons.org/ns#attributionName"),
+			new RDFLiteral(author), 'RDFa');
+	}
+
+	var authorUri = document.evaluate("//span[@class='realname']/a/@href",
+				       document,
+				       null,
+				       Components.interfaces.nsIDOMXPathResult.STRING_TYPE,
+				       null).stringValue;
+	if ("undefined" != typeof authorUri) {
+	    triples.add(new RDFSymbol(location),
+			new RDFSymbol("http://creativecommons.org/ns#attributionURL"),
+			new RDFSymbol(Util.uri.join(authorUri, location)), 
+			'RDFa');
+
+	}
     }); // flickr
