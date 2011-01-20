49044108430f0e062fb0033c09cf36665450dbb5
Nathan R. Yergler <nathan@yergler.net>
2011-01-19 22:10:08 -0800

    Include preference defaults in the generated XPI.

--- xpi.sh
+++ xpi.sh
@@ -4,4 +4,4 @@
 rm *.xpi
 
 # Pack all extension files
-zip -9 -r `basename \`pwd\``.xpi content locale module skin chrome.manifest install.rdf icon.png
+zip -9 -r `basename \`pwd\``.xpi content locale module skin defaults chrome.manifest install.rdf icon.png
